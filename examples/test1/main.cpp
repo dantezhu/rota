750bc10531affb13131b0c855aeaef343dc101bb
dantezhu <dantezhu@qq.com>
2014-05-20 17:02:45 +0800

    不知道为什么编译报错

--- examples/test1/main.cpp
+++ examples/test1/main.cpp
@@ -14,9 +14,95 @@
 #include "rota/actor.h"
 
 using namespace std;
+using namespace rota;
+
+#define FSM_IDLE  1
+#define FSM_RUN  2
+#define FSM_WALK  2
+
+class Player: public Actor {
+public:
+    Player() {}
+    virtual ~Player() {}
+    void idle() {
+        printf("player, %s\n", __func__);
+    }
+    void run() {
+        printf("player, %s\n", __func__);
+    }
+    void walk() {
+        printf("player, %s\n", __func__);
+    }
+};
+
+
+class FsmIdle: public Fsm
+{
+public:
+    FsmIdle() {}
+    virtual ~FsmIdle() {}
+
+    void enter(Actor* actor) {
+        printf("FsmIdle, %s\n", __func__);
+    }
+    int process(Actor* actor, float dt) {
+        printf("FsmIdle, %s\n", __func__);
+        ((Player*)actor)->idle();
+        return FSM_RUN;
+    }
+    void exit(Actor* actor) {
+        printf("FsmIdle, %s\n", __func__);
+        return;
+    }
+};
+
+class FsmRun: public Fsm
+{
+public:
+    FsmRun() {}
+    virtual ~FsmRun() {}
+
+    void enter(Actor* actor) {
+        printf("FsmRun, %s\n", __func__);
+    }
+    int process(Actor* actor, float dt) {
+        printf("FsmRun, %s\n", __func__);
+        ((Player*)actor)->run();
+        return FSM_WALK;
+    }
+    void exit(Actor* actor) {
+        printf("FsmRun, %s\n", __func__);
+        return;
+    }
+};
+
+class FsmWalk: public Fsm
+{
+public:
+    FsmWalk() {}
+    virtual ~FsmWalk() {}
+
+    void enter(Actor* actor) {
+        printf("FsmWalk, %s\n", __func__);
+    }
+    int process(Actor* actor, float dt) {
+        printf("FsmWalk, %s\n", __func__);
+        ((Player*)actor)->walk();
+        return FSM_IDLE;
+    }
+    void exit(Actor* actor) {
+        printf("FsmWalk, %s\n", __func__);
+        return;
+    }
+};
 
 int main(int argc, char **argv)
 {
-    printf("ok");
+    Fsm::dict[FSM_IDLE] = new FsmIdle();
+    Fsm::dict[FSM_RUN] = new FsmRun();
+    Fsm::dict[FSM_WALK] = new FsmWalk();
+
+    Player player;
+    player.changeState(FSM_IDLE);
     return 0;
 }
