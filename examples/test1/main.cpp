b338c59e38e231429174cceeb6a744a593afc143
dantezhu <dantezhu@qq.com>
2014-05-20 16:45:46 +0800

    test1

---
+++ examples/test1/main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+
+#include "rota/fsm.h"
+#include "rota/actor.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    printf("ok");
+    return 0;
+}
